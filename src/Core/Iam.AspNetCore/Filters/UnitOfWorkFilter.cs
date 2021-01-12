4bb484b9835008d7ea7bf4a267782617f7a12eff
huangzhengeng <huangzhengeng@xiekang.net>
2021-01-12 17:15:03 +0800

    add UnitOfWorkFilter

---
+++ src/Core/Iam.AspNetCore/Filters/UnitOfWorkFilter.cs
@@ -0,0 +1,46 @@
+﻿using Iam.Data.UnitOfWork;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iam.AspNetCore.Filters
+{
+    public class UnitOfWorkFilter : IAsyncActionFilter
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UnitOfWorkFilter(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            var controllerActionDescriptor = context.ActionDescriptor as ControllerActionDescriptor;
+
+            if(controllerActionDescriptor == null)
+            {
+                await next();
+                return;
+            }
+
+            var unitOfWorkAttr = controllerActionDescriptor.MethodInfo.GetCustomAttribute<UnitOfWorkAttribute>();
+            if(unitOfWorkAttr != null)
+            {
+                _unitOfWork.BeginTransaction();
+            }
+
+            var result = await next();
+
+            if (result.Exception == null || result.ExceptionHandled)
+            {
+                _unitOfWork.Commit();
+            }
+        }
+    }
+}
---
+++ src/Core/Iam.Data/UnitOfWork/UnitOfWorkAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iam.Data.UnitOfWork
+{
+    public class UnitOfWorkAttribute : Attribute
+    {
+        public UnitOfWorkAttribute(IsolationLevel isolationLevel = IsolationLevel.ReadUncommitted)
+        {
+            IsolationLevel = isolationLevel;
+        }
+
+        public IsolationLevel IsolationLevel { get; set; }
+    }
+}
