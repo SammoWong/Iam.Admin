c16516ae5250f2c2dc243bce24cdaca6bd4c2501
huangzhengeng <huangzhengeng@xiekang.net>
2021-01-18 18:04:51 +0800

    add ServiceDiscovery

---
+++ src/Core/Iam.ServiceDiscovery/ServiceInformation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Iam.ServiceDiscovery
+{
+    public class ServiceInformation
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Version { get; set; }
+
+        public string Host { get; set; }
+
+        public int Port { get; set; }
+
+        public IEnumerable<string> Tags { get; set; }
+
+        public Uri ToUri(string scheme = "http") => new Uri($"{scheme}://" + Host + ":" + Port);
+        
+    }
+}
