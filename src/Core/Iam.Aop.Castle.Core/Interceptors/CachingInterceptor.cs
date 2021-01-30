ad04da5edcd74f64609059ce55e1337e83dd2326
Sammo <sammo.wong@outlook.com>
2021-01-30 15:28:48 +0800

    add CachingInterceptor

---
+++ src/Core/Iam.Aop.Castle.Core/Interceptors/CachingInterceptor.cs
@@ -0,0 +1,72 @@
+﻿using Castle.DynamicProxy;
+using Iam.Core.Caching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Iam.Aop.Castle.Core.Interceptors
+{
+    public class CachingInterceptor : IInterceptor
+    {
+        private readonly ICache _cache;
+
+        public CachingInterceptor(ICache cache)
+        {
+            _cache = cache;
+        }
+
+        public void Intercept(IInvocation invocation)
+        {
+            var method = invocation.MethodInvocationTarget ?? invocation.Method;
+            var cachingAttr = method.GetCustomAttribute<CachingAttribute>();
+            //无此特性或者无返回值或者返回值为Task时，跳过此拦截器
+            if(cachingAttr == null || method.ReturnType == typeof(void) || method.ReturnType == typeof(Task))
+            {
+                invocation.Proceed();
+            }
+            else
+            {
+                //TODO：缓存的存取统一为string类型，再根据返回值做相应转换
+                var cacheKey = cachingAttr.CacheKey;
+                var expiration = cachingAttr.Expiration;
+                
+                var cacheValue = _cache.Get<string>(cacheKey);
+                if(cacheValue != null)
+                {
+                    var returnType = typeof(Task).IsAssignableFrom(method.ReturnType) 
+                        ? method.ReturnType.GenericTypeArguments.FirstOrDefault() 
+                        : method.ReturnType;
+
+                    dynamic result = JsonSerializer.Deserialize(cacheValue, returnType);
+                    invocation.ReturnValue = (typeof(Task).IsAssignableFrom(method.ReturnType)) ? Task.FromResult(result) : result;
+                    return;
+                }
+                //执行当前的方法
+                invocation.Proceed();
+                //存入缓存
+                if (!string.IsNullOrWhiteSpace(cacheKey))
+                {
+                    object response;
+                    var type = invocation.Method.ReturnType;
+                    if (typeof(Task).IsAssignableFrom(type))
+                    {
+                        var resultProperty = type.GetProperty("Result");
+                        response = resultProperty.GetValue(invocation.ReturnValue);
+                    }
+                    else
+                    {
+                        response = invocation.ReturnValue;
+                    }
+                    if (response == null) 
+                        response = string.Empty;
+
+                    _cache.TrySet(cacheKey, response, expiration);
+                }
+            }
+        }
+    }
+}
---
+++ src/Core/Iam.Core/Caching/CachingAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Iam.Core.Caching
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class CachingAttribute : Attribute
+    {
+        public CachingAttribute(string cacheKey)
+        {
+            CacheKey = cacheKey;
+        }
+
+        public CachingAttribute(string cacheKey, TimeSpan expiration)
+        {
+            CacheKey = cacheKey;
+            Expiration = expiration;
+        }
+
+        public string CacheKey { get; set; }
+
+        /// <summary>
+        /// 缓存过期时间
+        /// </summary>
+        public TimeSpan Expiration { get; set; } = TimeSpan.FromHours(1);
+    }
+}
